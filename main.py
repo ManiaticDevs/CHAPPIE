215b46a073864b756918c73e8ca0d0656ba2d8e3
Oikmo <genisis344@gmail.com>
2022-06-29 22:30:58 +0100

    update

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import time
 import warnings
 import logging
 
-
-
-time.sleep(5)
-
 warnings.filterwarnings("ignore")
 warnings.simplefilter('ignore')
 #warnings.filterwarnings("ignore", category=NotFound)
@@ -22,28 +18,66 @@ def progress_bar(current, total, bar_length=20):
 
     print(f'Progress: [{arrow}{padding}] {int(fraction*100)}%', end=ending)
 
-print("Setting up Python for Chappie (dependencies may not be on user's computer)")
-progress_bar(0, 100)
-os.system("py get_pip.py --user --no-warn-script-location --disable-pip-version-check>nul")
-import colorama 
-colorama.init()
-from colorama import Fore, Style, Back
+userASK = input("Have you already done the setup for chappie? (y or n, yes or no) : ")
 
-print(Fore.RED + "THE PROCESS YOU ARE ABOUT TO SEE IS THE TRAINING AND UPDATING PROCESS FOR CHAPPIE" + Style.RESET_ALL)
-progress_bar(25, 100)
-os.system("py updatejson.py && py train.py>nul")
-progress_bar(50, 100)
-os.system("pip install -r requirements.txt --user --no-warn-script-location --disable-pip-version-check>null")
-progress_bar(100, 100)
-time.sleep(1)
+userinp = userASK.lower()
 
+isSetup = False
+
+if userinp == 'no':
+    isSetup = False
+elif userinp == 'yes':
+    isSetup = True
+elif userinp == 'y':
+    isSetup = True
+elif userinp == 'n':
+    isSetup = False
+
+if isSetup == False:
+    os.system("cls")
+    print("Setting up Python for Chappie (dependencies may not be on user's computer)")
+    progress_bar(0, 100)
+    os.system("python3 get_pip.py --user --no-warn-script-location --disable-pip-version-check>nul")
+    progress_bar(25, 100)
+    os.system("pip install -r requirements.txt --user --no-warn-script-location --disable-pip-version-check>null")
+    progress_bar(50, 100)
+    os.system("python3 updatejson.py && python3 train.py>nul")
+    progress_bar(100, 100)
+    time.sleep(1)
+    os.system("cls")
+    os.system("python3 updatejson.py && python3 train.py>nul")
+
+userASK = input("Do you want to update the json file for chappie? (y or n, yes or no) : ")
+
+userinp = userASK.lower()
+
+isJSON = False
+
+if userinp == 'no':
+    isJSON = False
+elif userinp == 'yes':
+    isJSON = True
+elif userinp == 'y':
+    isJSON = True
+elif userinp == 'n':
+    isJSON = False
+
+if isJSON == True:
+    progress_bar(0, 100)
+    os.system("py updatejson.py>nul")
+    progress_bar(50, 100)
+    os.system("py train.py>nul")
+    progress_bar(100, 100)
+    time.sleep(2)
 
 import json
 import numpy as np
 from tensorflow import keras
 from sklearn.preprocessing import LabelEncoder
 
-
+import colorama
+colorama.init()
+from colorama import Fore, Style, Back
 
 import random
 import pickle
@@ -51,6 +85,10 @@ import pickle
 with open("intents.json") as file:
     data = json.load(file)
 
+#def writeinfile(n, filename):
+    #text_file = open(filename, "w")
+    #y = text_file.write(n)
+    #text_file.close()
 
 def chat():
     # load trained model
@@ -70,30 +108,24 @@ def chat():
     print(Fore.YELLOW + "Start messaging with Chappie (type quit to stop)!" + Style.RESET_ALL)
     print(Fore.RED + "PROPERTY OF MANIATICDEVS" + Style.RESET_ALL)
     print(Fore.GREEN + "Chappie :" + Style.RESET_ALL + " Hello! I'm Chappie! Note that my responses are limited!")
-    logging.basicConfig(
-    level=logging.INFO,
-    format="",
-    handlers=[
-        logging.FileHandler("debug.log"),
-        logging.StreamHandler(sys.stdout)
-        ]
-    )
+
     while True:
 
         print(Fore.LIGHTBLUE_EX + "User : " + Style.RESET_ALL, end="")
         inp = input()
-        logging.info(inp)
         if inp.lower() == "quit":
+            os.system("cls")
             break
 
         result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]), truncating='post', maxlen=max_len))
         tag = lbl_encoder.inverse_transform([np.argmax(result)])
-
+        print(" ", end="\r")
         for i in data['intents']:
             if i['tag'] == tag:
+
                print(Fore.GREEN + "Chappie :" + Style.RESET_ALL, np.random.choice(i['responses']))
             #else :
                #print(Fore.GREEN + "Chappie :" + Style.RESET_ALL, "I'm sorry I didn't get that.")
         # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
-        
+
 chat()
--- updatejson.py
+++ updatejson.py
@@ -6,8 +6,8 @@ nltk.download('punkt', quiet=True)
 with open("intents.json") as file:
     data = json.load(file)
 
-#-------------------------------------GREETINGS-----------------------------------#
-U1 = Article('https://chappie-webpages.werdimduly.repl.co/responses/greetings/user/')
+#-------------------------------------GREETINGS--------------------------------#
+U1 = Article('https://chappie-webpages.werdimduly.repl.co/greetings/user/')
 U1.download()
 U1.parse()
 U1.nlp()
@@ -15,7 +15,7 @@ corpusU1 = U1.text
 textU1 = corpusU1
 sentence_listU1 = nltk.sent_tokenize(textU1)
 
-R1 = Article('https://chappie-webpages.werdimduly.repl.co/responses/greetings/chappie/')
+R1 = Article('https://chappie-webpages.werdimduly.repl.co/greetings/chappie/')
 R1.download()
 R1.parse()
 R1.nlp()
@@ -23,8 +23,8 @@ corpusR1 = R1.text
 textR1 = corpusR1
 sentence_listR1 = nltk.sent_tokenize(textR1)
 
-#-------------------------------------GOODBYE-----------------------------------#
-U2 = Article('https://chappie-webpages.werdimduly.repl.co/responses/goodbye/user/')
+#-------------------------------------GOODBYE----------------------------------#
+U2 = Article('https://chappie-webpages.werdimduly.repl.co/goodbye/user/')
 U2.download()
 U2.parse()
 U2.nlp()
@@ -32,7 +32,7 @@ corpusU2 = U2.text
 textU2 = corpusU2
 sentence_listU2 = nltk.sent_tokenize(textU2)
 
-R2 = Article('https://chappie-webpages.werdimduly.repl.co/responses/goodbye/chappie/')
+R2 = Article('https://chappie-webpages.werdimduly.repl.co/goodbye/chappie/')
 R2.download()
 R2.parse()
 R2.nlp()
@@ -41,7 +41,7 @@ textR2 = corpusR2
 sentence_listR2 = nltk.sent_tokenize(textR2)
 
 #-------------------------------------THANKS-----------------------------------#
-U3 = Article('https://chappie-webpages.werdimduly.repl.co/responses/goodbye/user/')
+U3 = Article('https://chappie-webpages.werdimduly.repl.co/thanks/user/')
 U3.download()
 U3.parse()
 U3.nlp()
@@ -49,7 +49,7 @@ corpusU3 = U3.text
 textU3 = corpusU3
 sentence_listU3 = nltk.sent_tokenize(textU3)
 
-R3 = Article('https://chappie-webpages.werdimduly.repl.co/responses/goodbye/chappie/')
+R3 = Article('https://chappie-webpages.werdimduly.repl.co/thanks/chappie/')
 R3.download()
 R3.parse()
 R3.nlp()
@@ -57,16 +57,35 @@ corpusR3 = R3.text
 textR3 = corpusR3
 sentence_listR3 = nltk.sent_tokenize(textR3)
 
-#--------------------------------------UPDATE-------------------------------------#
+#-------------------------------------ABOUT------------------------------------#
+U4 = Article('https://chappie-webpages.werdimduly.repl.co/about/user/')
+U4.download()
+U4.parse()
+U4.nlp()
+corpusU4 = U4.text
+textU4 = corpusU4
+sentence_listU4 = nltk.sent_tokenize(textU4)
+
+R4 = Article('https://chappie-webpages.werdimduly.repl.co/about/chappie/')
+R4.download()
+R4.parse()
+R4.nlp()
+corpusR4 = R4.text
+textR4 = corpusR4
+sentence_listR4 = nltk.sent_tokenize(textR4)
+#--------------------------------------UPDATE----------------------------------#
 intents= []
 
-data['intents'][0]['patterns'] = sentence_listU1
+data['intents'][0]['patterns'] = sentence_listU1 #GREETINGS
 data['intents'][0]['responses'] = sentence_listR1
-data['intents'][1]['patterns'] = sentence_listU2
+data['intents'][1]['patterns'] = sentence_listU2 #GOODBYE
 data['intents'][1]['responses'] = sentence_listR2
-#data['intents'][1]['patterns'] = sentence_listU3
-#data['intents'][1]['responses'] = sentence_listR3
-
+data['intents'][2]['patterns'] = sentence_listU3 #THANKS
+data['intents'][2]['responses'] = sentence_listR3
+data['intents'][3]['patterns'] = sentence_listU4 #ABOUT
+data['intents'][3]['responses'] = sentence_listR4
+#data['intents'][4]['patterns'] = sentence_listU5 #
+#data['intents'][4]['responses'] = sentence_listR5
 
 with open('intents.json', 'w') as file:
     json.dump(data, file, indent = 4)
